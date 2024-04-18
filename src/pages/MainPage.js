dc1f5293a8294eb0158573d61c8fe6f89afac289
dnscjf <fkdlxm666@naver.com>
2024-04-18 13:19:28 +0900

    input 상태 관리하기

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,14 +1,30 @@
 import React from "react";
+import Counter from "../componets/Counter";
+import CounterReducer from "../componets/CounterReducer";
+import InputReducer from "../componets/InputReducer";
 
 const MainPage = () => {
   return (
     <div>
-        <h1>Hooks study</h1>
-      <h1>메인 페이지입니다.</h1>
-      <div>
+      <h1>Hooks study</h1>
+      {/* <div>
         <h2>useState</h2>
         <h3>예시: 카운터 앱</h3>
         <Counter />
+      </div> */}
+      {/* <div>
+        <h2>useEffect</h2>
+        <h3>예시: 카운터 앱</h3>
+        <Counter />
+      </div> */}
+      {/* <div>
+        <h2>useReducer</h2>
+        <h3>예시: 리듀서 활용 카운터 앱</h3>
+        <CounterReducer />
+      </div> */}
+      <div>
+        <h3>예시: input 상태 관리하기</h3>
+        <InputReducer />
       </div>
     </div>
   );
