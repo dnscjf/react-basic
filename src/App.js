57970cf27b3a041ac87903e265260f5ea08a2cca
dnscjf <fkdlxm666@naver.com>
2024-04-25 11:09:37 +0900

    docs: Axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import ScssComponent from "./components/ScssComponent";
 import StyledComponent from "./components/StyledCompononet";
+import Axios from "./pages/Axios";
 
 
 function App() {
   return (
     <div>
-      <StyledComponent />
+      <Axios />
     </div>
   );
 }
